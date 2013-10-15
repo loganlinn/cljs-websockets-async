a9fff4e34e17eb5bac0c08fb87a96ef047d97caa
loganlinn <logan.linn@gmail.com>
2013-10-15 18:03:30 -0500

    Avoid nested go blocks
    Minimize go block boundary to prevent compilation issues

--- src/cljs_websockets_async/core.cljs
+++ src/cljs_websockets_async/core.cljs
@@ -1,46 +1,42 @@
 (ns cljs-websockets-async.core
   (:require [cljs.reader :refer [read-string]]
-            [cljs.core.async :as async :refer [<! >! chan close! put! take! sliding-buffer dropping-buffer timeout]])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [cljs.core.async :as async :refer [<! >! chan close! put!]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (defn connect!
-  "Connects to a websocket.
-  Options:
-  :in  - a fn called w/ no arguments to obtain a core.async channel for input (optional, default: cljs.core.async/chan)
-  :out - a fn called w/ no arguments to obtain a core.async channel for output (optional, default: cljs.core.async/chan)
-
-  Returns a channel that, when connected, puts a map with with keys,
-  :ws  - Raw Websocket object
-  :in  - Channel to write values to socket on
-  :out - Channel to recieve socket data on"
+  "Connects to a websocket. Returns a channel that, when connected, puts a
+  map with with keys,
+  :uri  The URI connected to
+  :ws   Raw Websocket object
+  :in   Channel to write values to socket on
+  :out  Channel to recieve socket data on"
   ([uri] (connect! uri {}))
   ([uri {:keys [in out] :or {in chan out chan}}]
    (let [on-connect (chan)]
-     (go
-       (let [in (in)
-             out (out)
-             ws (js/WebSocket. uri)]
-         (doto ws
-           (aset "onopen"
-                 (fn []
-                   (close! on-connect)
-                   (go (loop []
-                         (let [data (<! in)]
-                           (if-not (nil? data)
-                             (do (.send ws (pr-str data))
-                                 (recur))
-                             (do (close! out)
-                                 (.close ws))))))))
-           (aset "onmessage"
-                 (fn [m]
-                   (when-let [data (read-string (.-data m))]
-                     (put! out data))))
-           (aset "onclose"
-                 (fn []
-                   (close! in)
-                   (close! out))))
+     (let [in (in)
+           out (out)
+           ws (js/WebSocket. uri)]
+       (doto ws
+         (aset "onopen"
+               (fn []
+                 (close! on-connect)
+                 (go-loop []
+                   (let [data (<! in)]
+                     (if-not (nil? data)
+                       (do (.send ws (pr-str data))
+                           (recur))
+                       (do (close! out)
+                           (.close ws)))))))
+         (aset "onmessage"
+               (fn [m]
+                 (when-let [data (read-string (.-data m))]
+                   (put! out data))))
+         (aset "onclose"
+               (fn []
+                 (close! in)
+                 (close! out))))
 
+       (go
          (<! on-connect)
-
          ;; swap in/out for more intuitive naming for receiver
-         {:ws ws :out in :in out})))))
+         {:uri uri :ws ws :out in :in out})))))
